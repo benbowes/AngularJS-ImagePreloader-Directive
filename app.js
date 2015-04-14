9742a837d7b032a742f0725074932a134b424657
Ben Bowes <bb@benbowes.com>
2015-04-14 22:50:03 +1000

    Add comments to directive

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 var app = angular.module('ImagePreloader', [])
 
   .directive('preloadimg', function() {
+
     return {
       restrict: 'A',
       replace: true,
       link: function(scope, element, attrs) {
-
-        var tempImg = new Image();
-        element.addClass('preload-img')
+        
+        var tempImg = new Image(); // create a new Image object
+        element.addClass('preload-img') // set initial classes
         element.addClass('preload-img--preloading')
         element.wrap( "<div class='preload-img__wrap'></div>" );
         
-        tempImg.onload = function(){
+        tempImg.onload = function(){ // fires when image has fully downloaded
           element.removeClass('preload-img--preloading');
+          tempImg = null; // delete temporary image
         }
-        tempImg.src = attrs.ngSrc;
+        tempImg.src = attrs.ngSrc; // start loading image
       }
     };
   });
-
